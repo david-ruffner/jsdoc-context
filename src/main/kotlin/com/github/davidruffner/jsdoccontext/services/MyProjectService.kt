43a8964c0004c3f28eb5d80f8c83ccefabb62617
GitHub Action <action@github.com>
2025-07-27 23:23:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/davidruffner/jsdoccontext/services/MyProjectService.kt
+++ src/main/kotlin/com/github/davidruffner/jsdoccontext/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.davidruffner.jsdoccontext.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.davidruffner.jsdoccontext.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
